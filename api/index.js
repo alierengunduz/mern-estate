0510553e864b0f54dcc55bb9cc2f370566e8a7ce
Ali Eren Gündüz <aegakademi@gmail.com>
2024-05-07 23:20:42 +0300

    coonect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
-
-
+import connectDB from './db/config.js';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 3000;
 
 
 
@@ -12,7 +13,7 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 
 } );
-
+ connectDB();
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 } );
