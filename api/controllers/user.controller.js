6244b84baea9f07ceeeb6a7ea2cb287150c82ba3
Ali Eren Gündüz <aegakademi@gmail.com>
2024-05-08 10:20:56 +0300

    create a test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,10 @@
+
+
+const test = (req, res) => {
+    res.json({ message: 'User route test' });
+}
+
+
+export {
+    test
+}
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import connectDB from './db/config.js';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,10 +10,9 @@ const port = process.env.PORT || 3000;
 
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+// Routers
+app.use('/api/users', userRouter);
 
-} );
  connectDB();
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
