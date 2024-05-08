ad3e10b9787462f2ff472e8bda72e19b71ab7730
Ali Eren Gündüz <aegakademi@gmail.com>
2024-05-08 21:24:31 +0300

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,25 +1,21 @@
 import UserModel from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import {errorHandler} from '../utils/error.js';
-
+import jwt from 'jsonwebtoken';
 
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 
 const signup = async (req, res,next) => {
-  try {
-    const { username, email, password } = req.body;
- 
-      
+  const { username, email, password } = req.body;
+  try {      
     if (!emailRegex.test(email)) {
-      return res.status(400).json({ message: "Invalid email format" });
+      return next(errorHandler(400,"Invalid email format"));
     } 
 
     const existingUser = await UserModel.findOne({ email });
     if (existingUser) {
-      return res
-        .status(400)
-        .json({ message: "User with this email already exists" });
+      return next(errorHandler(400,"User already exists"));
     }
 
     const salt = await bcryptjs.genSalt(10);
@@ -39,4 +35,36 @@ const signup = async (req, res,next) => {
   }
 };
 
-export { signup };
+
+const signin = async(req,res,next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await UserModel.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404,"User not found"));
+    }
+
+    const isPasswordCorrect = await bcryptjs.compare(password, validUser.password);
+    if (!isPasswordCorrect) {
+      return next(errorHandler(401,"Wrong credentials"));
+    }
+    // Token'i oluşturuyoruz
+    const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET,{expiresIn:'1d'});
+    // Kullanıcının şifresini haslenmiş olsa bile görmek istemiyoruz
+    const {password:pass,...rest} = validUser._doc;
+   // Token'i cookide saklıyoruz
+   //HttpOnly:true ile sadece server tarafından okunabilir olmasını sağlıyoruz
+    res.cookie('access_token',token,{
+      httpOnly:true,
+      maxAge:24*60*60*1000 // 1 day
+    });
+    res.status(200).json({ 
+      message: "User signed in successfully",
+      rest,
+     });
+  } catch (error) {
+    next(error)
+  }
+}
+
+export { signup, signin};
