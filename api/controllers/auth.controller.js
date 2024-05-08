a64c92eaff2a494b637815265762d3173518a3ca
Ali Eren Gündüz <aegakademi@gmail.com>
2024-05-08 17:08:40 +0300

    emailRegex upload

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,20 @@
 import UserModel from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import {errorHandler} from '../utils/error.js';
+
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+
 const signup = async (req, res,next) => {
   try {
     const { username, email, password } = req.body;
  
- 
+      
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    } 
+
     const existingUser = await UserModel.findOne({ email });
     if (existingUser) {
       return res
