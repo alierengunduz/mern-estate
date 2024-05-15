b1d9fd0d5592a7843a159e550322ff8043562a4e
Ali Eren Gündüz <aegakademi@gmail.com>
2024-05-15 10:50:13 +0300

    create  update lissting api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,33 +1,72 @@
-import {errorHandler} from "../utils/error.js"
-import Listing from "../models/listing.model.js"
+import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listing.model.js";
+import mongoose from "mongoose";
+
+const isValidObjectId = (id) => {
+  return mongoose.Types.ObjectId.isValid(id);
+};
 
 const createListing = async (req, res, next) => {
-   try {
-    const listing = await Listing.create(req.body)
-    res.status(201).json(listing)
-   } catch (error) {
-    next(error)
-   }
-} 
-
+  try {
+    const listing = await Listing.create(req.body);
+    res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
 
 const deleteListing = async (req, res, next) => {
-  const listing  = await Listing.findById(req.params.id)
-   if (!listing) {
-      return next(errorHandler(404,"Listing not found"))
-   }
+  const { id } = req.params;
+
+  if (!isValidObjectId(id)) {
+    return next(errorHandler(400, "Invalid ObjectId"));
+  }
+
+  const listing = await Listing.findById(id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+
   if (req.user.id !== listing.userRef) {
-      return next(errorHandler(401,"Your can only delete your own listings"))
-   } 
-   try {
-      await Listing.findByIdAndDelete(req.params.id)
-      res.status(200).json('Listing has been deleted')
-   } catch (error) {
-      next(error)
-   }
-}
+    return next(errorHandler(401, "You can only delete your own listings"));
+  }
+
+  try {
+    await Listing.findByIdAndDelete(id);
+    res.status(200).json('Listing has been deleted');
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateListing = async (req, res, next) => {
+  const { id } = req.params;
+
+  if (!isValidObjectId(id)) {
+    return next(errorHandler(400, "Invalid ObjectId"));
+  }
+
+  const listing = await Listing.findById(id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own listings"));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export {
-   createListing,
-   deleteListing
-}
+  createListing,
+  deleteListing,
+  updateListing
+};
