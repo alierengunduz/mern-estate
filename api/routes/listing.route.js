a426bdfdd9536547298983427438dd50ec0de337
Ali Eren Gündüz <aegakademi@gmail.com>
2024-05-16 13:39:17 +0300

    create search API route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 const router = express.Router();
 import { verifyToken } from "../utils/verifyUser.js";
-import { createListing,deleteListing,updateListing,getListing } from "../controllers/listing.controller.js";
+import { createListing,deleteListing,updateListing,getListing,getListings } from "../controllers/listing.controller.js";
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
 // listeleri herkes görebilir
 router.get('/get/:id', getListing);
+router.get('/get', getListings);
 export default router;
