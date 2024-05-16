6d637811664cfb1a487e7dd6c3ffd9844fe5c74d
Ali Eren Gündüz <aegakademi@gmail.com>
2024-05-16 16:32:30 +0300

    create search page UI and functionality to the search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Profile from "./pages/Profile"
 import Listing from "./pages/Listing"
+import Search from "./pages/Search"
 import Header from "./components/Header"
 import PrivateRoute from "./components/PrivateRoute"
 import CreateListing from "./pages/CreateListing"
@@ -19,6 +20,7 @@ const App = () => {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/listing/:listingId" element={<Listing />} />
+        <Route path="/search" element={<Search />} />
         <Route element={<PrivateRoute/>} >
         <Route path="/profile" element={<Profile />} />
         <Route path="/create-listing" element={<CreateListing />} />
